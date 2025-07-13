d759e4a9536fb28d3cd45219f6b26080f9f6fdae
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-07-13 06:40:11 +0100

    made some small changes in Calculator.pp

--- Calculator.cpp
+++ Calculator.cpp
@@ -8,48 +8,58 @@ int main()
     double num1;
     double num2;
     double result;
+    char Continue = 'y';
 
-    cout << "Enter an operator ( + , - , / , x ) : ";
-    cin >> Operator;
-
-    while(Operator != '+' && Operator != '-' && Operator != '/' && Operator != 'x'  && Operator != 'X'){
-        cout << "Please enter a valid operator ( + , - , / , x ) : ";
+    while(Continue == 'y' || Continue == 'Y'){
+        cout << "Enter an operator ( + , - , / , x ) : ";
         cin >> Operator;
+
+        while(Operator != '+' && Operator != '-' && Operator != '/' && Operator != 'x'  && Operator != 'X'){
+            cout << "Please enter a valid operator ( + , - , / , x ) : ";
+            cin >> Operator;
+        }
+
+        cout << "Enter the first number : ";
+        while(!(cin >> num1)){
+            cin.clear();
+            cin.ignore(1000,'\n');
+            cout << "Please enter a valid number : ";
+        }
+
+        cout << "Enter the second number : ";
+        while(!(cin >> num2)){
+            cin.clear();
+            cin.ignore(1000,'\n');
+            cout << "Please enter a valid number : ";
+        }
+
+
+    
+
+        switch(Operator){
+            case '+':
+                result = num1 + num2;
+                break;
+            case 'x':
+                result = num1 * num2;
+                break;
+            case 'X':
+                result = num1 * num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '/':
+                if(Operator == '/' && num2 == 0){
+                    cout << "Can't be divided by zero";
+                    return 0;
+                }
+                result = num1 / num2;
+                break;
+        }
+        cout << "The result of " << num1 << " " << Operator << " " << num2 <<  " is " << result << endl;
+        cout << "Do you want to calculate again (y/n)";
+        cin >> Continue;
     }
-
-    cout << "Enter the first number : ";
-    while(!(cin >> num1)){
-        cin.clear();
-        cin.ignore(1000,'\n');
-        cout << "Please enter a valid number : ";
-    }
-
-    cout << "Enter the second number : ";
-    while(!(cin >> num2)){
-        cin.clear();
-        cin.ignore(1000,'\n');
-        cout << "Please enter a valid number : ";
-    }
-
-    switch(Operator){
-        case '+':
-            result = num1 + num2;
-            break;
-        case 'x':
-            result = num1 * num2;
-            break;
-        case 'X':
-            result = num1 * num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '/':
-            result = num1 / num2;
-            break;
-    }
-    cout << "The result of " << num1 << " " << Operator << " " << num2 <<  " is " << result;
-
-
     return 0;
 }
