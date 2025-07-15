6ed257196d6c0ae23dd6949dcaed4eaf523697e6
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-07-15 09:35:44 +0100

    solved some logic issues

--- Practice/NumberGuess.cpp
+++ Practice/NumberGuess.cpp
@@ -15,15 +15,17 @@ int main()
     computer = (rand() % 6) + 1;
 
     while(true){
-        cout << "Choose a random number between 1 to 6";
+        cout << "Choose a random number between 1 to 6: ";
         getline(cin,playerString);
 
         stringstream ss(playerString);
-        if(ss >> player && ss.eof()){
-            break;
+        if(ss >> player && ss.eof() && player > 1 && player < 6 ){
+           break;
         }
         cout << "Invalid input. Try again\n";
     }
+    cout << "Your choice : " << player;
+    cout << "Computer : " << computer;
 
     return 0;
 }
