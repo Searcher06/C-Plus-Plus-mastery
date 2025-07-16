ea8dfd3b8dff3c128b5259b9989eff173783bdc8
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-07-16 01:45:02 +0100

    built a number guessing game

--- Practice/NumberGuess.cpp
+++ Practice/NumberGuess.cpp
@@ -9,23 +9,48 @@ int main()
     int computer;
     string playerString;
     int player;
+    int score = 0;
+    int computerScore = 0;
+    int trial = 3;
+    string Continue;
 
-    srand(time(0));
-    // Generating random number between 1 - 6;
-    computer = (rand() % 6) + 1;
+    while(trial >= 1){
+        srand(time(0));
+        // Generating random number between 1 - 6;
+        computer = (rand() % 6) + 1;
 
-    while(true){
-        cout << "Choose a random number between 1 to 6: ";
-        getline(cin,playerString);
+        while(true){
+            cout << "Choose a random number between 1 to 6: ";
+            getline(cin,playerString);
 
-        stringstream ss(playerString);
-        if(ss >> player && ss.eof() && player > 1 && player < 6 ){
-           break;
+            stringstream ss(playerString);
+            if(ss >> player && ss.eof() && player >= 1 && player <= 6 ){
+               break;
+            }
+            cout << "Invalid input. Try again\n";
         }
-        cout << "Invalid input. Try again\n";
+
+        if(computer == player){
+            score ++;
+            cout << "You guessed the right number!";
+        } else {
+            computerScore ++;
+            cout << "Failed to guess the right number ! \n";
+        }
+        cout << "Your choice : " << player << '\n';
+        cout << "Computer : " << computer << '\n' << '\n';
+
+        cout << "*************** Scores *************** \n" << '\n';
+        cout << "Your score : " << score << endl;
+        cout << "computer : " << computerScore << '\n' << endl;
+        trial--;
     }
-    cout << "Your choice : " << player;
-    cout << "Computer : " << computer;
+    if(score > computerScore){
+        cout << "Congratulations you won!\n";
+    }else{
+        cout << "Computer won, you failed! \n";
+    }
+    
 
     return 0;
 }
