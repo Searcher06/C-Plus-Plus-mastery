96a639ab5287067d7ad4fba4168feb2e21d7e848
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-07-27 23:55:37 +0100

    Structs

---
+++ structs.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Struct : A structure that groups related variable under one name
+    // structs can contain many different data types (strings,int,char)
+    // variables in a struct are known as "members"
+    // members can access with . "class member access operator"
+
+    system("pause");
+    return 0;
+}
