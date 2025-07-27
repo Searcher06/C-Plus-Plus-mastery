00073b1d51b3ce09147c37f80a868488c240237b
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-07-27 23:51:12 +0100

    learned about function templates

--- functionTemplates.cpp
+++ functionTemplates.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 using namespace std;
+template <typename T, typename U>
+auto maximum(T x, U y)
+{
+    return (x > y) ? x : y;
+}
+// Function template describes what a function looks like.
+// can be used to generate as many overloaded function as
+// needed, each using different data types
 
 int main()
 {
-
+    cout << maximum(1, 3.5) << '\n';
     return 0;
 }
