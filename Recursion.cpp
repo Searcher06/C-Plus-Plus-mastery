d500c85531567b124c87f864dc42e2cbd4971732
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-07-27 23:31:06 +0100

    function templates

--- Recursion.cpp
+++ Recursion.cpp
@@ -7,13 +7,14 @@ int main()
     // Recursion = a programming technique where a function
     //              invokes itself from within
     //              break a complex concept into a repeatable single steps
-    walk(8);
+    walk(3);
     return 0;
 }
 void walk(int steps)
 {
-    for (int i = 0; i < steps; i++)
+    if (steps > 0)
     {
         cout << "You take a step!\n";
+        walk(steps - 1);
     }
 }
