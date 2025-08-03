6535a66b31eb93b551e27426c1bd696cf81bec68
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-08-03 02:30:25 +0100

    inheritance in cpp

---
+++ Inheritance.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+
+public:
+    bool alive = true;
+    void eat()
+    {
+        cout << "This animal is eating \n";
+    }
+};
+
+class Dog : public Animal
+{
+public:
+    void bark()
+    {
+        cout << "The dog goes woof! \n";
+    }
+};
+
+class Cat : public Animal
+{
+public:
+    void meow()
+    {
+        cout << "The cat goes meow \n";
+    };
+};
+int main()
+{
+    Dog dog;
+    Cat cat;
+
+    cout << dog.alive << "\n";
+    dog.eat();
+    dog.bark();
+
+    cout << cat.alive << "\n";
+    cat.eat();
+    cat.meow();
+    return 0;
+}
